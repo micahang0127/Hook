39f092e25694e76d9b9828908eccb9213d335e85
micahang0127 <psa5013@hanmail.net>
2020-04-12 21:28:04 +0900

    useEffect 예제

--- src/component/Info.js
+++ src/component/Info.js
@@ -1,9 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
 
+  useEffect(() => {
+      console.log('렌더링 완료');
+      console.log({ name, nickname });
+  });
   const onChangeName = e => {
     setName(e.target.value);
   };
@@ -14,6 +18,7 @@ const Info = () => {
 
   return (
     <div style={{ border:'solid 1px darkblue', padding:'20px', margin:'10px'}}>
+        <h2>[ useEffect ]</h2>
       <div>
         <input value={name} onChange={onChangeName} />
         <input value={nickname} onChange={onChangeNickname} />
@@ -27,6 +32,7 @@ const Info = () => {
           {nickname}
         </div>
       </div>
+      console.log확인하기
     </div>
   );
 };
